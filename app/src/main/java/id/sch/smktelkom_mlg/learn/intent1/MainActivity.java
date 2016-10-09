1632aff419b4b991a3195ecd858132a755982a19
Pradigma Ayu Naratryka <pradigma.ayu.n@gmail.com>
2016-10-09 20:21:55 +0700

    startActivity Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
@@ -4,17 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NAMA = "Nama";
+    public static final String UMUR = "Umur";
+    EditText etNama;
+    EditText etUmur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Input");
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etUmur = (EditText) findViewById(R.id.editTextUmur);
 
         findViewById(R.id.buttonHitung).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                goHasil();
             }
         });
 
@@ -24,5 +35,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, HasilActivity.class));
             }
         });
+    }
+
+    private void goHasil() {
+        String nama = etNama.getText().toString();
+        String umurStr = etUmur.getText().toString();
+
+        int umur = umurStr.isEmpty() ? 0 : Integer.parseInt(umurStr);
+
+        Intent intent = new Intent(MainActivity.this, HasilActivity.class);
+        intent.putExtra(NAMA, nama);
+        intent.putExtra(UMUR, umur);
+
+        startActivity(new Intent(MainActivity.this, HasilActivity.class));
     }
 }
